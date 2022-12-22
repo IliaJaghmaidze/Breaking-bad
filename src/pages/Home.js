5971655cc24312b6c257d67c9a5509357d056802
Glthub-owner <iliajaghmaidze@gmail.com>
2022-12-22 22:52:00 +0400

    Added wallpaper for homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,10 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { increment } from "../redux/characterSlice";
+import Wallpaper from "../assets/wallpaper.png";
 
 function Home() {
-  // const character = useSelector((state) => state.character.value);
-  // const status = useSelector((state) => state.character.status);
-  // const list = useSelector((state) => state.character.list);
-  // const [items, setNew] = React.useState(["A", "B", "C"]);
-  // const dispatch = useDispatch();
-  // console.log(status);
-  // console.log(list);
-  const handleClick = (event, key) => {
-    console.log(event.target);
-    console.log("key index: ", key);
-  };
   return (
-    <div className="App">
-      {/* <h1>{character}</h1>
-      {items.map((item) => (
-        <button key={item.toString()} onClick={() => dispatch(increment(item))}>
-          {item}
-        </button>
-        // <button onClick={(event) => handleClick(event, key)} key={key}>
-        //   {item}
-        // </button>
-      ))} */}
-      <Button>sHE</Button>
+    <div className="image-container">
+      <img src={Wallpaper} className="wallpaper-img" />
     </div>
   );
 }
