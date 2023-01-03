d1d003b722b16a26bbb313bdf2b04bbff267331f
Glthub-owner <iliajaghmaidze@gmail.com>
2023-01-03 16:44:45 +0400

    Updated homepage title and its background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ function Home() {
   return (
     <div className="image-container">
       <img src={Wallpaper} className="wallpaper-img" />
-      <div class="center">Centered</div>
+      <div class="center">
+        Welcome to Breaking Bad & Better Call Saul Character Wiki
+      </div>
     </div>
   );
 }
