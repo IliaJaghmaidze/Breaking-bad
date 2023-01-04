4d9d4d3440d82c7d540b3884b5d67564dae266c1
Glthub-owner <iliajaghmaidze@gmail.com>
2023-01-04 18:04:06 +0400

    Added introduction page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Wallpaper from "../assets/wallpaper.png";
-
+import background from "../assets/background.jpg";
+import Jesse from "../assets/jesse.PNG";
+import Walter from "../assets/walter.PNG";
+import { Container } from "react-bootstrap";
 function Home() {
   return (
-    <div className="image-container">
-      <img src={Wallpaper} className="wallpaper-img" />
-      <div class="center">
-        Welcome to Breaking Bad & Better Call Saul Character Wiki
+    <Container>
+      <div className="welcome">
+        <img src={Wallpaper} className="wallpaper-img" alt="wallpaper" />
+        <div class="center">
+          Welcome to Breaking Bad & Better Call Saul Character Wiki
+        </div>
       </div>
-    </div>
+      <div className="about">
+        <img src={background} className="background-img" alt="wallpaper" />
+        <div className="cards-container">
+          <img src={Walter} className="walter-card" />
+          <img src={Jesse} className="jesse-card" />
+        </div>
+      </div>
+      <div className="favourites"></div>
+    </Container>
   );
 }
 
